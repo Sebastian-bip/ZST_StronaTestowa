e8d86e678a4f272c03403713a8549e96d6292074
Sebastian-bip <sebiks.bastion@onet.pl>
2025-12-25 23:43:22 +0100

    Usinięcie tokenów do bazy danych z kodu bo pod żadnym pozorem nie powinno go tam być :)

--- src/app/api/api-test.js
+++ src/app/api/api-test.js
@@ -11,6 +11,4 @@ export const POST = async () => {
   //const result = await client.execute("CREATE TABLE todos (description);");
 };
 
-//tokens:
-// eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicm8iLCJpYXQiOjE3NjY5NDQwMjksImlkIjoiY2EyMGJkZmQtZjgyZS00OTdjLWI2YTMtNTEyMjE0YjI5ODk0IiwicmlkIjoiN2ZkY2RkMzYtMjkwNi00ZTBiLWJkYjItNzVlZGIzMGYzNmMxIn0.mEcRrOoj0YRjW4DLkTd1dh7jLJ8mSa_Vu4-ZwEUM5sZXJWe632D84UuB_u_ZoLf-xj4YD4T0kn5p52B8Vn7nCQ
-// libsql://podrzeczna-vercel-icfg-gffwqo9ppxkftx5bbuprqm3g.aws-eu-west-1.turso.io
+//tokens: tokeny zostały usunięte hihi
