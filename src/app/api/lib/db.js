7713935fa34511d0107d0924985aaa98dc6af0c0
Sebastian-bip <sebiks.bastion@onet.pl>
2025-12-31 13:39:27 +0100

    Fix merge conflict in db connection

--- src/app/api/lib/db.js
+++ src/app/api/lib/db.js
@@ -3,12 +3,11 @@ import { createClient } from "@libsql/client";
 export const db = createClient({
   url:DATABASE_URL,
   authToken:DATABASE_AUTH_TOKEN,
-<<<<<<< HEAD
-  //url:TURSO_DATABASE_URL,
-  //authToken:TURSO_AUTH_TOKEN,
-=======
-  // url:TURSO_DATABASE_URL,
-  // authToken:TURSO_AUTH_TOKEN,
->>>>>>> 7cd6df2a7d8cfe79ba5258f733a119ac6926bfd2
-}); // naprawione połączenie po stronie kuirwa obuuu
+}); 
+// Komentarze nie mogą być w obiektach
+//url:TURSO_DATABASE_URL,
+//authToken:TURSO_AUTH_TOKEN,
+
+// naprawione połączenie po stronie kuirwa obuuu
 // kurwa do pierwszej siedzę
+//31 stycznia pokurwi mnie xD
