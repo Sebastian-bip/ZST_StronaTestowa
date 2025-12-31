442984839903f82064c9fbcd08db94915a3d418a
Sebastian-bip <sebiks.bastion@onet.pl>
2025-12-31 14:06:29 +0100

    fix env runtime

--- src/app/api/lib/db.js
+++ src/app/api/lib/db.js
@@ -1,9 +1,10 @@
 import { createClient } from "@libsql/client";
 
 export const db = createClient({
-  url:DATABASE_URL,
-  authToken:DATABASE_AUTH_TOKEN,
-}); 
+  url:process.env.TURSO_DATABASE_URL,
+  authToken:process.env.TURSO_AUTH_TOKEN,
+});
+
 // Komentarze nie mogą być w obiektach
 //url:TURSO_DATABASE_URL,
 //authToken:TURSO_AUTH_TOKEN,
