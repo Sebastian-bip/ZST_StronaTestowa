71ddca1ff323bd0d66912bc40bb11a85d8dfd46e
Sebastian-bip <sebiks.bastion@onet.pl>
2025-12-31 14:10:56 +0100

    fix env runtime

--- src/app/(pages)/data-base-test/page.jsx
+++ src/app/(pages)/data-base-test/page.jsx
@@ -1,6 +1,11 @@
+export const dynamic = "force-dynamic";
+
 export default async function DbTest() {
   //const res = await fetch("http://localhost:3000/api/data", {
-  const res = await fetch("/api/users");
+  const res = await fetch("/api/users", {
+  cache: "no-store",
+});
+
 
   const data = await res.json();
   // ten div w returnie nie obrazi się na styl xD
