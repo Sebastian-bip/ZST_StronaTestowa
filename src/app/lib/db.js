118fb3919c9b7b0e0f411279f948047b838435a5
Maksymilian Sławiński <loox17@Maksymilians-MacBook-Pro.local>
2025-12-31 01:31:37 +0100

    poprawa strony testowej do bazy danych

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,9 +1,9 @@
 import { createClient } from "@libsql/client";
 
 export const db = createClient({
-  //url: process.env.DATABASE_URL,
-  //authToken: process.env.DATABASE_AUTH_TOKEN,
-  url:TURSO_DATABASE_URL,
-  authToken:TURSO_AUTH_TOKEN,
+  url: process.env.DATABASE_URL,
+  authToken: process.env.DATABASE_AUTH_TOKEN,
+  // url:TURSO_DATABASE_URL,
+  // authToken:TURSO_AUTH_TOKEN,
 }); // naprawione połączenie po stronie kuirwa obuuu
 // kurwa do pierwszej siedzę
