ae73a7a89c3fe6bf92251f5a70e52a625a1dbe4f
Sebastian-bip <sebiks.bastion@onet.pl>
2025-12-31 01:59:15 +0100

    zmiena kluczy na serwerowe zamiast lokalnych

--- src/app/api/lib/db.js
+++ src/app/api/lib/db.js
@@ -1,8 +1,8 @@
 import { createClient } from "@libsql/client";
 
 export const db = createClient({
-  url: process.env.DATABASE_URL,
-  authToken: process.env.DATABASE_AUTH_TOKEN,
+  url:DATABASE_URL,
+  authToken:DATABASE_AUTH_TOKEN,
 <<<<<<< HEAD
   //url:TURSO_DATABASE_URL,
   //authToken:TURSO_AUTH_TOKEN,
