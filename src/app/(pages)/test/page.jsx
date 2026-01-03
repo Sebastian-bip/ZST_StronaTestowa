8ea69b94718a401c2d091f69a1a869470be222c8
Maksymilian Sławiński <loox17@Maksymilians-MacBook-Pro.local>
2026-01-03 19:34:58 +0100

    rzygi i sraka kurwa

--- src/app/(pages)/test/page.jsx
+++ src/app/(pages)/test/page.jsx
@@ -1,10 +1,17 @@
+import "./test.css"
 
+export const metadata = {
+    title: "CSS testing site"
+}
 
 export default function Test() {
     return(
         <>
             <div>
-                test
+                <img hidden src="https://images.pexels.com/photos/33229939/pexels-photo-33229939.jpeg" alt=""/>
+                <div className="test-container">
+                    <div className="test-content">Test</div>
+                </div>
             </div>
         </>
     )
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import "./page.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="mainBlock">
         Strona główna
+          <Link href="/test">test</Link>
     </div>
   );
 }
