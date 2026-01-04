30c25108a876cd233c59a35d394be0a6b173ab4d
Sebastian-bip <sebiks.bastion@onet.pl>
2026-01-04 14:23:34 +0100

    Dodanie dwóch nowych funkcji
    Funkcja wypisująca 4 ostatnie artykuły
    Funkcja wypisuje cały artykuł wraz z jej autorem i jej danymi

---
+++ src/app/api/api-test.js
@@ -0,0 +1,16 @@
+import { createClient } from '@libsql/client';
+import { NextResponse } from 'next/server';
+
+const client = createClient({
+  url: process.env.TURSO_DATABASE_URL,
+  authToken: process.env.TURSO_AUTH_TOKEN
+});
+
+export const POST = async () => {
+  // Fetch data from SQLite
+  const result = await client.execute("CREATE TABLE todos (description);");
+};
+
+//tokens:
+// eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicm8iLCJpYXQiOjE3NjY5NDQwMjksImlkIjoiY2EyMGJkZmQtZjgyZS00OTdjLWI2YTMtNTEyMjE0YjI5ODk0IiwicmlkIjoiN2ZkY2RkMzYtMjkwNi00ZTBiLWJkYjItNzVlZGIzMGYzNmMxIn0.mEcRrOoj0YRjW4DLkTd1dh7jLJ8mSa_Vu4-ZwEUM5sZXJWe632D84UuB_u_ZoLf-xj4YD4T0kn5p52B8Vn7nCQ
+// libsql://podrzeczna-vercel-icfg-gffwqo9ppxkftx5bbuprqm3g.aws-eu-west-1.turso.io
