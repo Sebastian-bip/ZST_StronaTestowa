1165fa2613fd8b5df1183d1d0ff2c106081c95fc
Sebastian-bip <sebiks.bastion@onet.pl>
2026-01-04 17:24:28 +0100

    Revert "poprawa stabilności mojego projektu"
    This reverts commit 6054608d8780be3b8c07d8f5641b9724b708556f.

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -1,5 +1,5 @@
 export const dynamic = "force-dynamic";
-import { db } from "@/app/api/lib/db";
+import { db } from "../lib/db";
 
 export async function GET() {
   try {
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,6 @@
 import Image from "next/image";
 import "./page.css";
 
-export const metadata = {
-    title: "db tests"
-}
-
 export default function Home() {
   return (
     <div className="mainBlock">
